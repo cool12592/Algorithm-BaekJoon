237f50d56be21e9250598ace4a97d6987faf5b2a
cool12592 <122303897+cool12592@users.noreply.github.com>
2023-04-23 22:53:46 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    if (n == 1)
+        cout << 'B';
+    else
+        cout << 'A';
+    return 0;
+}
